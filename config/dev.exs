fd61c40270ab08cb5c1d562be8278f977c561a67
Aka-5 <Teden542639>
2021-10-05 00:00:59 +0200

    cleannup:add database

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ import Config
 
 # Configure your database
 config :training, Training.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "admin",
   database: "training_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
